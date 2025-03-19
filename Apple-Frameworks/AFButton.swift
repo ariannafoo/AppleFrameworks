f43e630f853bf2a52ce96e20ee9cbdfb9dba8189
Arianna Foo <fooar@sheridancollege.ca>
2025-03-19 16:59:40 -0400

    Creating UI for Framework Detail View

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by Arianna Foo on 2025-03-19.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
