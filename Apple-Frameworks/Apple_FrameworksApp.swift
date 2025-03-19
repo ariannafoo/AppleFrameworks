f6bce521e6e68244e000633886f531f69de12de5
Arianna Foo <fooar@sheridancollege.ca>
2025-03-19 13:57:29 -0400

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Arianna Foo on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
